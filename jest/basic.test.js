f692c7d21af4103bcad022b771341981ae9831a1
Alberto Martinez de Murga Ramirez <alberto@threkk.com>
2022-08-08 23:09:00 +0200

    feat: adds some examples using the describe syntax

--- jest/basic.test.js
+++ jest/basic.test.js
@@ -1,8 +1,6 @@
 import { expect, test } from '@jest/globals'
 import assert from 'node:assert'
 
-// Edit an assertion and save to see HMR in action
-
 test('Math.sqrt()', () => {
   expect(Math.sqrt(4)).toBe(2)
   expect(Math.sqrt(144)).toBe(12)
--- nodejs/suite.test.js
+++ nodejs/suite.test.js
@@ -1,23 +1,22 @@
-import test from 'node:test'
+import { describe, it } from 'node:test'
 import assert from 'node:assert'
 import { expect } from 'chai'
 
-test('sub tests', async (t) => {
-  await t.test('foo', () => {
+describe('sub tests', () => {
+  it('foo', () => {
     assert.equal(Math.sqrt(4), 2)
   })
 
-  await t.test('bar', () => {
+  it('bar', () => {
     expect(1 + 1).eq(2)
   })
 })
 
-test('skip', (t) => {
-  t.skip('this test will be skipped')
-  expect(true).eq(false)
-})
+describe('other functionalities', () => {
+  it.skip('skip', () => {
+    expect(true).eq(false)
+  })
 
-test('todo', (t) => {
-  t.todo('this test is a todo')
-  expect(true).eq(false)
+  it.todo('todo', () => {
+  })
 })
